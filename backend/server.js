3006e500f4f4d56fb63ef7e67aa5e851ac54a16c
Muhammad Yusuf <yusuftkr077@gmail.com>
2024-07-05 11:12:16 +0700

    video 4 selesai

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -19,10 +20,17 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts',workoutRoutes)
 
-
-
-// listen for requests
+// connect to db
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    // listen for requests
 app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000', process.env.PORT)
+    console.log('connected to db & listening on port', process.env.PORT)
 })
+})
+.catch((error) => {
+    console.log(error)
+})
+
+
 
