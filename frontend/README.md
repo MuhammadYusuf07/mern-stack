87294345deb3a7f1c34dc1431ef9cfa7619733e2
Muhammad Yusuf <yusuftkr077@gmail.com>
2024-07-20 15:46:38 +0700

    selesai

--- frontend/README.md
+++ frontend/README.md
@@ -68,3 +68,13 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+**cek signup**
+![signup](image.png)
+
+**Cek Login di web aplication**
+![login aplication console](image-1.png)
+
+**Sudah Login**
+![sudah login](image-2.png)
+
--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,8 +1,10 @@
 import { useState } from "react"
-import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const WorkoutForm = () => {
     const { dispatch } = useWorkoutsContext()
+    const { user } = useAuthContext()
 
     const [title, setTitle] = useState('')
     const [load, setLoad] = useState('')
@@ -13,13 +15,19 @@ const WorkoutForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
 
+        if (!user) {
+            setError('You must be logged in')
+            return
+        }
+
         const workout = { title, load, reps }
 
         const response = await fetch('/api/workouts', {
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${user.token}`
             }
         })
         const json = await response.json()
@@ -34,18 +42,15 @@ const WorkoutForm = () => {
             setReps('')
             setError(null)
             setEmptyFields([])
-            console.log('new workout added', json)
             dispatch({ type: 'CREATE_WORKOUT', payload: json })
         }
-
     }
-
 
     return (
         <form className="create" onSubmit={handleSubmit}>
-            <h3>Tambah Workout Baru</h3>
+            <h3>Add a New Workout</h3>
 
-            <label>Exersize Title : </label>
+            <label>Excersize Title:</label>
             <input
                 type="text"
                 onChange={(e) => setTitle(e.target.value)}
@@ -53,7 +58,7 @@ const WorkoutForm = () => {
                 className={emptyFields.includes('title') ? 'error' : ''}
             />
 
-            <label>Load (in kg): </label>
+            <label>Load (in kg):</label>
             <input
                 type="number"
                 onChange={(e) => setLoad(e.target.value)}
@@ -61,7 +66,7 @@ const WorkoutForm = () => {
                 className={emptyFields.includes('load') ? 'error' : ''}
             />
 
-            <label>Reps: </label>
+            <label>Reps:</label>
             <input
                 type="number"
                 onChange={(e) => setReps(e.target.value)}
@@ -69,7 +74,7 @@ const WorkoutForm = () => {
                 className={emptyFields.includes('reps') ? 'error' : ''}
             />
 
-            <button>Tambah Workout</button>
+            <button>Add Workout</button>
             {error && <div className="error">{error}</div>}
         </form>
     )
